26ecfe598a955ef5822f48bb025dd90e47624bd4
guepardeau <hassanakhalid22@gmail.com>
2025-12-15 14:41:11 +0000

    validate user input for time and applied_per_year and add final output

--- compound-interest/main.py
+++ compound-interest/main.py
@@ -1,3 +1,5 @@
+from compound import compound
+
 def get_valid_float(user_input):
             while True:
                 try: 
@@ -19,11 +21,40 @@ principal = ((input("Please enter starting balance. (e.g 10000): ")))
 principal = get_valid_float(principal)
 interest_rate = (input("Please enter the interest rate (e.g 2.5): "))
 interest_rate = get_valid_float(interest_rate)
-time = input("How many years will the money be growing? (e.g 15 for 15 years): ")
-time = get_valid_float(time)
-applied_per_year = (input("How often will interest be paid out, daily, monthly or yearly?: "))
-applied_per_year = applied_per_year.strip().lower()[0]
-applied_per_year = get_valid_choice(applied_per_year, {"d","m","y"})
+interest_rate = interest_rate/100
+
+while True:
+   try:
+        time = float(input("How many years will the money be growing? (e.g 15 for 15 years): "))
+        if time > 0:
+            break
+        else:
+            print("You cannot have negative years. Enter a number greater than 0: ")
+   except ValueError:
+        print("Time must be numerical.")
+
+while True:
+    applied_per_year = (input("How often will interest be paid out, daily, monthly or yearly?: ").strip().lower())
+    if applied_per_year in ["daily", "monthly", "yearly"]:
+        break
+    else:   
+        print(f"{applied_per_year} is not valid.")
+
+if applied_per_year == "daily":
+      applied_per_year = 365
+elif applied_per_year == "monthly":
+      applied_per_year = 12
+elif applied_per_year == "yearly":
+      applied_per_year = 1
+else:
+      pass
+
+amount = compound(principal, interest_rate, applied_per_year, time)
+difference = amount - principal
+growth_factor = (amount/principal)
+growth_percentage = (growth_factor-1)*100
+
+print(f"The final amount after {time} years would be {currency}{amount:.2f}. You would earn {currency}{difference:.2f}. A growth of {growth_percentage:.2f}%.")
 
 
 
