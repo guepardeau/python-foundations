aeb8b90f4a567bd257b9e71fadd247c757a8c488
guepardeau <hassanakhalid22@gmail.com>
2025-12-17 19:03:06 +0000

    initialise basket dict and a dd exit program feature

--- shopping-cart/main.py
+++ shopping-cart/main.py
@@ -1,28 +1,49 @@
+basket = {}
+
+print("YOUR SHOPPING CART")
+print(" ")
+
 while True:
-    item = str(input("Enter Item: ").lower())
-    if item.isalpha():
+    while True:
+        item = str(input("Enter Item: ").lower())
+        if item.isalpha():
+            break
+        else:
+            print(("ERROR: Item must be Alphabetical (e.g. Apples)"))
+
+    while True:
+        try:
+            price = float(input("Enter Item Price:"))
+            if price > 0:
+                break
+            else:
+                print("ERROR: Price must be greater than 0.") 
+                continue
+        except ValueError:
+            print("ERROR: Price must be numerical: ")
+
+    while True:
+        try:
+            qty = int(input("Enter Item Quantity: "))
+            if qty > 0:
+                break
+            else:
+                print("ERROR: Quantity must be greater than 0.") 
+                continue
+        except ValueError:
+            print("ERROR: Quantity must be an integer (e.g. 4 for 4 apples): ")
+
+    basket[item] = {
+        "price":price,
+        "quantity": qty
+    }
+
+    end_shopping = input("Would you like to add another item? (Y/N): ").upper()
+    if end_shopping == "Y":
+        continue
+    elif end_shopping == "N":
         break
     else:
-        print(("ERROR: Item must be Alphabetical (e.g. Apples)"))
+        print("ERROR: Please choose Y/N: ")
 
-while True:
-    try:
-        price = float(input("Enter Item Price:"))
-        if price > 0:
-            break
-        else:
-            print("ERROR: Price must be greater than 0.") 
-            continue
-    except ValueError:
-        print("ERROR: Price must be numerical: ")
-
-while True:
-    try:
-        qty = int(input("Enter Item Quantity: "))
-        if qty > 0:
-            break
-        else:
-            print("ERROR: Quantity must be greater than 0.") 
-            continue
-    except ValueError:
-        print("ERROR: Quantity must be an integer (e.g. 4 for 4 apples): ")
+print(basket)
